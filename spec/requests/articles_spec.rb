2fa4647051471a72582a93402fbcbaf42c1686ae
Jakub Arnold <darthdeus@gmail.com>
2011-10-31 16:05:23 +0100

    Added some controller specs for authentication

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,23 +1,20 @@
 require 'spec_helper'
 
 describe "Articles" do
-  describe "POST /articles" do
-    it "should require logged in user" do
-      # get '/log_out'      
-      # category_id = create(:category, name: "lipsum").id
+  describe "POST /articles" do    
+    it "should create an article for a user who is logged in" do      
+      user = create(:user)      
+      post sessions_path, :username => user.username, :password => user.password
       
-      post '/articles'
-      page.should have_content "You have to be logged in"
-    end
-    
-    it "should create an article" do
-      create(:category, name: "lipsum")
-      visit new_article_path
-      fill_in "Title", :with => "Lorem Ipsum"
-      fill_in "Text", :with => "Lorem ipsum dolor sit amet"
-      select "lipsum", :from => "article_category_id"
-      click_button "Publish"
-      page.should have_content("Article published")
+      category = create(:category)
+
+      post articles_path, :article => { :title => "lorem", :text => "ipsum", :category_id => category.id }
+      response.should redirect_to(articles_path)
+      
+      follow_redirect!
+      
+      response.should render_template(:index)
+      response.body.should include("published")
     end
   end
 end
