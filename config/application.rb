635d900c7d85ecb91f01e7da6400d9753089ac00
Jakub Arnold <darthdeus@gmail.com>
2011-11-16 19:20:24 +0100

    Fixed fontface and added basic design structure

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,7 @@ module Scvrush
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
