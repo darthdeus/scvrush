72eeba54048941b44368165cc83c99f4c631e31e
Jakub Arnold <darthdeus@gmail.com>
2011-11-17 10:25:00 +0100

    Added styles for article heading

--- db/migrate/20111116164602_create_posts.rb
+++ db/migrate/20111116164602_create_posts.rb
@@ -3,12 +3,9 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :content
-      t.references :category
 
       t.timestamps
     end
-
-    add_index :posts, :category_id
 
   end
 end
