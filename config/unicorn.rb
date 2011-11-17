8ea9aa35c7003d355a4f84819122db75b67737d2
Jakub Arnold <darthdeus@gmail.com>
2011-11-17 02:14:53 +0100

    Updated unicorn.rb config with capistrano paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/var/apps/scvrush"
-pid "/var/apps/scvrush/tmp/pids/unicorn.pid"
-stderr_path "/var/apps/scvrush/log/unicorn.log"
-stdout_path "/var/apps/scvrush/log/unicorn.log"
+working_directory "/var/apps/scvrush.com/current"
+pid "/var/apps/scvrush.com/shared/pids/unicorn.pid"
+stderr_path "/var/apps/scvrush.com/shared/log/unicorn.log"
+stdout_path "/var/apps/scvrush.com/shared/log/unicorn.log"
 
 listen "/tmp/unicorn.scvrush.sock"
 worker_processes 2
