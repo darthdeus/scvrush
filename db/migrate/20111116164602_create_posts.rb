8820b1171c8764ee466a40a9c3850483de4e8df0
Jakub Arnold <darthdeus@gmail.com>
2011-11-17 11:09:14 +0100

    Added maintenance page and updated nginx to serve it from the main domain

--- db/migrate/20111116164602_create_posts.rb
+++ db/migrate/20111116164602_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.string :featured_image
 
       t.timestamps
     end
