bed8a287df2566e8955d2ae79572ba0e9fd58541
Jakub Arnold <darthdeus@gmail.com>
2011-11-18 21:50:10 +0100

    Added validations to forum models

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,5 +2,7 @@ class Reply < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   
+  validates :topic, :presence => true
+  validates :user, :presence => true
   validates :content, :presence => true
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ActiveRecord::Base
   belongs_to :section
   belongs_to :user
   has_many :replies
-  
+
+  validates :user, :presence => true
+  validates :section, :presence => true
   validates :name, :presence => true
 end
