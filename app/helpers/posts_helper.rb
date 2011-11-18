376fc85f46cb96c70447a14b53ac0405fc3446a5
Jakub Arnold <darthdeus@gmail.com>
2011-11-18 18:52:10 +0100

    Added default featured image

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,12 @@
 module PostsHelper
   def featured_image(post)
-    base_url = "http://scvrush.superinzerce.com/wp-content/uploads/"
-    img = image_tag(base_url + post.featured_image, :alt => post.featured_image)
-    link_to img, post    
+    unless post.featured_image
+      image_tag "http://scvrush.com/wp-content/themes/Avenue/timthumb.php?src=http://scvrush.com/wp-content/themes/Avenue/images/thumbnail.png&w=100&h=100", :alt => "No image available"
+    else
+      base_url = "http://scvrush.superinzerce.com/wp-content/uploads/"
+      img = image_tag(base_url + post.featured_image, :alt => post.featured_image)
+      link_to img, post    
+    end
   end
   
   def post_excerpt(content)
