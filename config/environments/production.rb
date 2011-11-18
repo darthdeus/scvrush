969b84ae4ab0231b09c43f4107f64a119acd739f
Jakub Arnold <darthdeus@gmail.com>
2011-11-18 04:31:41 +0100

    Added default url for action mailer for development and production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Scvrush::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => "scvrush.com" }
+
 end
