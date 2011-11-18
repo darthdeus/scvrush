eec6f6b1ef085103e6cb42b250bdf1b893226975
Jakub Arnold <darthdeus@gmail.com>
2011-11-18 02:20:59 +0100

    Refactored header into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# encoding: utf-8
 module ApplicationHelper
+  def arrowed_header(text)
+    "<h2 class='arrowed'>#{text}<span class='arrows'>»</span></h2>".html_safe
+    # content_tag(:h2, :class => "arrowed") {
+    #   ("Recent posts" + content_tag(:span, "»", :class => "arrows")).html_safe
+    # }
+  end
 end
