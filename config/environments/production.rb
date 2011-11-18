bb9f37591c4ea31f9f4111fcdb48ad662a6fcfdc
Jakub Arnold <darthdeus@gmail.com>
2011-11-18 04:43:21 +0100

    Added ActionMailer sendmail configuration for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,9 @@ Scvrush::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => "scvrush.com" }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  
 
 end
