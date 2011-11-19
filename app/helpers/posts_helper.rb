3b5102ca1c7041c5f6e3a1b41077cb1278e7a9c3
Jakub Arnold <darthdeus@gmail.com>
2011-11-19 11:39:56 +0100

    Updated link for pictures to point to S3 storage

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 module PostsHelper
   def featured_image(post)
     unless post.featured_image
-      image_tag "http://scvrush.com/wp-content/themes/Avenue/timthumb.php?src=http://scvrush.com/wp-content/themes/Avenue/images/thumbnail.png&w=100&h=100", :alt => "No image available"
+      image_tag "http://scvrush.superinzerce.com/wp-content/themes/Avenue/timthumb.php?src=http://scvrush.superinzerce.com/wp-content/themes/Avenue/images/thumbnail.png&w=100&h=100", :alt => "No image available"
     else
-      base_url = "http://scvrush.superinzerce.com/wp-content/uploads/"
+      base_url = "https://s3.amazonaws.com/scvrush/pictures/"
       img = image_tag(base_url + post.featured_image, :alt => post.featured_image)
       link_to img, post    
     end
