e1e2c0dde68455866f05da34f9a9baed8b8f0f5b
Jakub Arnold <darthdeus@gmail.com>
2011-11-25 01:28:23 +0100

    updated url for the image helper and removed bundle exec from the init file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,8 +3,9 @@ module PostsHelper
     unless post.featured_image
       image_tag "http://scvrush.superinzerce.com/wp-content/themes/Avenue/timthumb.php?src=http://scvrush.superinzerce.com/wp-content/themes/Avenue/images/thumbnail.png&w=100&h=100", :alt => "No image available"
     else
-      base_url = "https://s3.amazonaws.com/scvrush/pictures/"
-      img = image_tag(base_url + post.featured_image, :alt => post.featured_image)
+      # base_url = "https://s3.amazonaws.com/scvrush/pictures/"
+      # img = image_tag(base_url + post.featured_image, :alt => post.featured_image)
+      img = image_tag post.featured_image_url(:thumb), :alt => post.title
       link_to img, post    
     end
   end
