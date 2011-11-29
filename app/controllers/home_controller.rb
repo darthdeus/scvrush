cfd1a4b74fdc4bec293c83ca21df8827e31c3626
Jakub Arnold <darthdeus@gmail.com>
2011-11-29 23:41:51 +0100

    added zerg show to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.published.tagged_with('even,tournament', :exclude => true).page(0).limit(6)
+    @posts = Post.published.tagged_with('even,tournament,zerg-show', :exclude => true).page(0).limit(6)
     @eu_tournaments = Post.tagged_with('event,eu').limit(3)
     @na_tournaments = Post.tagged_with('event,na').limit(3)
     @tournaments = @eu_tournaments.zip(@na_tournaments).flatten
+    @zerg = Post.published.tagged_with('zerg-show').page(0).limit(6)    
   end
 
 end
