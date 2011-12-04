8d999f4656f98975dc89f7271f6ad1110967a8b0
Jakub Arnold <darthdeus@gmail.com>
2011-12-04 15:07:24 +0100

    added tag_list to form edit and basic spec for password reset

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe PasswordResetsController do
   describe "#create" do
     it "redirects user to signup_path when specified invalid url" do
-      pending
-      User.stub!(:find_by_username).and_return(nil)
-      post :create
-      flash[:notice].should contain("The user with given email doesn't exist")
+      User.should_receive(:find_by_email).with("invalid@example.com").and_return(nil)
+      post :create, :email => "invalid@example.com"
+      response.should redirect_to(signup_path)
+      flash[:notice].should match("The user with given email doesn't exist")
     end
   end
 end
