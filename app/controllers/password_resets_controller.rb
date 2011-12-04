04923e2e16bef23caadee6fae41ae98c8880c3d5
Jakub Arnold <darthdeus@gmail.com>
2011-12-04 12:45:49 +0100

    added message to inform user when they try to reset a password for a non-existing account

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,8 +4,12 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    redirect_to root_path, :notice => "An email has been sent with instructions how you can reset your password. It might take a couple of minutes before you receive the email, please don't forget to check your spam folder."
+    if user
+      user.send_password_reset
+      redirect_to root_path, :notice => "An email has been sent with instructions how you can reset your password. It might take a couple of minutes before you receive the email, please don't forget to check your spam folder."
+    else
+      redirect_to signup_path, :notice => "The user with given email doesn't exist. It is possible we lost your username during the data transfer, please sign up again - you can use the exact same username you had before."
+    end
   end
   
   def edit
