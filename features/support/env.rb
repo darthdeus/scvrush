7e256100336d6e80961aed8c73bf83610a61cb94
Jakub Arnold <darthdeus@gmail.com>
2011-12-07 19:09:35 +0100

    cucumber now reloads all models

--- features/support/env.rb
+++ features/support/env.rb
@@ -56,4 +56,13 @@ Spork.each_run do
   #     DatabaseCleaner.strategy = :transaction
   #   end
   #
+
+  require 'factory_girl_rails'
+  # reload all the models
+  Dir["#{Rails.root}/app/models/**/*.rb"].each do |model|
+    load model
+  end
+  
+  Scvrush::Application.reload_routes!
+
 end
