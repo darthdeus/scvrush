cd2cf6377bf10a949e26b3d9194468e4835c0429
Jakub Arnold <darthdeus@gmail.com>
2011-12-17 23:25:24 +0100

    point now requires a user

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ActiveRecord::Base
   belongs_to :user
-  
   belongs_to :reason, :polymorphic => true
-  
+
+  validates_presence_of :user
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -4,4 +4,9 @@ describe Point do
   it "has a valid factory" do
     build(:point).should be_valid
   end
+
+  it 'requires a user' do
+    build(:point, user: false).should_not be_valid
+  end
+
 end
