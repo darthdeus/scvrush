fe008a073ce745a77453f117130990e8a0470fc5
Jakub Arnold <darthdeus@gmail.com>
2011-12-20 20:30:17 +0100

    fixed helpers for generating buttons to use correct paths and http method

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,12 +1,12 @@
 module TournamentsHelper
   def signup_button(tour)
     button_to "Sign up for the tournament",
-      { :controller => :signups, :action => :create },
-        :method => :post, :id => tour.id, :class => 'btn'
+      { :controller => :signups, :action => :create, :id => tour.id },
+        :method => :post, :class => 'btn'
   end
 
   def checkin_button(tour)
-    button_to "Checkin!", 
+    button_to "Checkin!",
       { :controller => :signups, :action => :update, :id => tour.id },
         :method => :put, :class => 'btn'
   end
