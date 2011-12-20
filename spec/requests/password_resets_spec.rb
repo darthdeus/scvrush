b9a98a74e9dd85477a9f113478242c1895059a58
Jakub Arnold <darthdeus@gmail.com>
2011-12-20 13:06:36 +0100

    reverted back to working version of the password reset spec

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,7 +1,16 @@
+require 'spec_helper'
+
+describe "PasswordResets" do
+  it "emails user when requesting password reset" do
+    user = Factory(:user)
+    visit login_path
+    click_link "password"
+    fill_in "Email", :with => user.email
+
     click_button "Reset Password"
 
     current_path.should eq(root_path)
-    page.should have_content("email has been")
+    page.should have_content("Email sent")
 
     last_email.to.should include(user.email)
   end
