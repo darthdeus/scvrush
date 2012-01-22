aeda1ac7d5d890c7b24ee0f9d09ee923b32f5efd
Jakub Arnold <darthdeus@gmail.com>
2012-01-22 22:18:55 +0100

    Added blog post administration

---
+++ spec/factories/blog_posts.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :blog_post do
+    title "MyString"
+    url "MyString"
+    order 1
+  end
+end
