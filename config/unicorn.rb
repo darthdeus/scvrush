6ee51832e35490578991bbcd21e57c9b229132d6
Jakub Arnold <darthdeus@gmail.com>
2012-02-13 22:26:05 +0100

    Tweaked unicorn config to use proper bundle path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,21 @@ stderr_path "/var/apps/scvrush.com/shared/log/unicorn.log"
 stdout_path "/var/apps/scvrush.com/shared/log/unicorn.log"
 
 listen "/tmp/unicorn.scvrush.sock"
-worker_processes 2
+worker_processes 4
 timeout 30
+
+application = "scvrush.com"
+app_path = "/var/apps/#{application}"
+bundle_path = "#{app_path}/shared/bundle"
+
+before_exec do |_|
+  paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
+  paths.unshift "#{bundle_path}/bin"
+
+  ENV["PATH"] = paths.uniq.join(File::PATH_SEPARATOR)
+  ENV["GEM_HOME"] = ENV['GEM_PATH'] = bundle_path
+  ENV["BUNDLE_GEMFILE"] = "#{app_path}/current/Gemfile"
+end
+
 
 # preload_app true
