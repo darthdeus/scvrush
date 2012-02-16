1477cbe14ca54c3b9598e812b53ffb18ff96c0dc
Jakub Arnold <darthdeus@gmail.com>
2012-02-16 17:05:56 +0100

    Fixed session store to use mem_cache_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Scvrush::Application.config.session_store :cookie_store, key: '_scvrush_session'
-Scvrush::Application.config.session_store :redis_store
+Scvrush::Application.config.session_store :mem_cache_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
