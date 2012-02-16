9c07994c6163fffe099f3757f18ea00b9d8ec617
Jakub Arnold <darthdeus@gmail.com>
2012-02-16 18:09:32 +0100

    Added site verification for google webmaster tools and reduced unicorn processes to 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ stderr_path "/var/apps/scvrush.com/shared/log/unicorn.log"
 stdout_path "/var/apps/scvrush.com/shared/log/unicorn.log"
 
 listen "/tmp/unicorn.scvrush.sock"
-worker_processes 4
+worker_processes 2
 timeout 30
 
 application = "scvrush.com"
