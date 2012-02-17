beb2a74c8a71d58ba6c6cdc8afc59f904c53e10b
Jakub Arnold <darthdeus@gmail.com>
2012-02-17 18:19:56 +0100

    Changed unicorn processes to 4 and put in custom asset compilation that shouldn't recompile unless they change

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,17 @@ role :db,  domain, :primary => true
 
 namespace :deploy do
 
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
   # task :cold do
   #   update
   #   load_schema
