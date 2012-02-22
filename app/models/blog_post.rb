515c3acc5ccf080a9a9318ca22bd999c43458db4
Jakub Arnold <darthdeus@gmail.com>
2012-02-22 16:47:58 +0100

    Added validations to blog posts

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,6 @@
 class BlogPost < ActiveRecord::Base
+  validates_presence_of :title, :url, :order
+
   scope :recent, order('`order` DESC').limit(5)
 
   before_save :expire_sidebar_cache
--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe BlogPost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    build(:blog_post).should be_valid
+  end
+
+  it "requires a title" do
+    post = build(:blog_post, title: nil)
+    post.should have_at_least(1).error_on(:title)
+  end
+
+  it "requires a url" do
+    post = build(:blog_post, url: nil)
+    post.should have_at_least(1).error_on(:url)
+  end
+
+  it "requires a order" do
+    post = build(:blog_post, order: nil)
+    post.should have_at_least(1).error_on(:order)
+  end
 end
