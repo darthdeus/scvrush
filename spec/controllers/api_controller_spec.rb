9846f56daec183e833b29d2e90ae7443a79d4084
Jakub Arnold <darthdeus@gmail.com>
2012-03-10 10:44:40 +0100

    Added more specs for the API controller

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -3,17 +3,36 @@ require 'spec_helper'
 describe ApiController do
 
   describe "GET 'auth'" do
-    it "returns http success" do
-      get 'auth'
-      response.should be_success
+    it "returns unauthorized for no params" do
+      get :auth, format: 'json'
+      response.status.should == 401
     end
+
+    it "returns the API key if user is authorized" do
+      user = stub(generate_api_key!: 'foobar', api_key: nil)
+      User.stub!(:authenticate) { user }
+      get :auth, format: 'json'
+    end
+
   end
 
   describe "GET 'check'" do
-    it "returns http success" do
-      get 'check'
-      response.should be_success
+    it "returns unauthorized for no params" do
+      get :check, format: 'json'
+      response.status.should == 401
     end
+
+    it "returns 200 if the API key exists" do
+      User.stub!(:find_by_api_key) { true }
+      get :check, format: 'json'
+      response.status.should == 200
+    end
+
+    it "returns 401 for non-existing API key" do
+      get :check, format: 'json', api_key: 'foobar'
+      response.status.should == 401
+    end
+
   end
 
 end
