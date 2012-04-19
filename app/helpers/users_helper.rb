93663c7e0172bd03b671aeb4b1c2ac0fae765143
Jakub Arnold <darthdeus@gmail.com>
2012-04-19 17:01:51 +0200

    Update bootstrap to 2.0.2 + basics for the new user profile design

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,29 +1,31 @@
 module UsersHelper
-  def user_info(user, attr)
-    if user.read_attribute(attr).present?
-      content_tag :li do
-        "#{attr.titleize}: #{user.send(attr)}".html_safe        
-      end
-    end
-  end  
-  
+
   def conditional_user_info(user, attr)
     if user.read_attribute("display_#{attr}")
       user_info(user, attr)
     end
-  end  
-  
+  end
+
+  def user_info(user, attr)
+    if user.read_attribute(attr).present?
+      out = ""
+      out += content_tag :dt, attr.titleize
+      out += content_tag :dd, user.send(attr)
+      out.html_safe
+    end
+  end
+
   def info_for_user(user, info, conditional_info)
     output = ""
     info.each do |attr|
       data = user_info(user, attr)
       output += data if data
-    end    
+    end
     conditional_info.each do |attr|
       data = conditional_user_info(user, attr)
       output += data if data
     end
-    
+
     output.html_safe
   end
 end
