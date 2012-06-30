2cd05b3d32636b0ae0df6d75ce3fa5d1e70f28d2
Jakub Arnold <darthdeus@gmail.com>
2012-06-30 19:01:25 +0200

    Add ember.js

---
+++ app/assets/javascripts/controllers/application_controller.js
@@ -0,0 +1,4 @@
+Scvrush.ApplicationController = Ember.ObjectController.extend({
+  // Implement your controller here.
+});
+
---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,5 @@
+Scvrush.Store = DS.Store.extend({
+  revision: 4,
+  adapter: DS.RESTAdapter.create()
+});
+
