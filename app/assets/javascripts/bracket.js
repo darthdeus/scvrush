0d977730b9baaa24f851076feb17f4f9635e3d12
Jakub Arnold <darthdeus@gmail.com>
2012-07-01 21:36:55 +0200

    Add basic tournament layout with AngularJS

--- app/assets/javascripts/bracket.js
+++ app/assets/javascripts/bracket.js
@@ -1,8 +1,68 @@
-//= require scvrush
+function BracketCtrl($scope) {
+  $scope.rounds = [
+    {
+      type: "round",
+      round: 8,
+      number: 1,
+      matches: [
+        { player1: "ham", player2: "burger" },
+        { player1: "jack", player2: "john" },
+        { player1: "kara", player2: "tiara" },
+        { player1: "tara", player2: "lara" }
+      ]
+    },
+
+    {
+      type: "round",
+      round: 4,
+      number: 2,
+      matches: [
+        { player1: "ham", player2: "jack" },
+        { player1: "kara", player2: "lara" }
+      ]
+    },
+
+    {
+      type: "round",
+      round: 2,
+      number: 3,
+      matches: [
+        { player1: "ham", player2: "kara" }
+      ]
+    }
+  ];
+
+  $scope.clickPlayer = function(player) {
+    debugger;
+  };
+
+
+}
 
 jQuery(function($) {
 
-  var bracket = $('#js-bracket');
+  $.fn.applyDimensions = function(dim) {
+    var recalculateDimensions = function(dim) {
+      return {
+        playerHeight: dim.playerHeight,
+        playerBox: dim.playerBox,
+        playerGap: dim.playerGap + dim.playerBox + dim.matchGap + 4,
+        matchGap: dim.playerBox + dim.matchGap + dim.playerGap,
+        roundTop: (dim.playerBox / 2) + (dim.playerGap / 2) + dim.roundTop
+      };
+    };
+
+    $(this).find('.round').each(function(round) {
+      $(this).css('marginTop', dim.roundTop);
+
+      $(this).find('.player').css('height', dim.playerHeight);
+      var matches = $(this).find('.match');
+      matches.css('marginBottom', dim.matchGap);
+
+      matches.find('.player:first').css('marginBottom', dim.playerGap);
+      dim = recalculateDimensions(dim);
+    });
+  }
 
   var dimensions = {
     playerHeight: 20,
@@ -12,62 +72,23 @@ jQuery(function($) {
     roundTop: 0
   };
 
-  var players = 4,
-      matches = players / 2;
+  $('.bracket').applyDimensions(dimensions);
 
+  $('.bracket .player').live({
+    mouseenter: function() {
+      var cls = $(this)[0].className;
+      // TODO - add backwards compatible #map
+      cls = cls.split(' ').map(function(n) { return "." + n; }).join("")
+      $(cls).addClass('player-highlight');
+    },
 
-  var recalculateDimensions = function(dim) {
-    return {
-      playerHeight: dim.playerHeight,
-      playerBox: dim.playerBox,
-      playerGap: dim.playerGap + dim.playerBox + dim.matchGap + 4,
-      matchGap: dim.playerBox + dim.matchGap + dim.playerGap,
-      roundTop: (dim.playerBox / 2) + (dim.playerGap / 2) + dim.roundTop
-    };
-  }
-
-  var html = {
-      round : function(dim) {
-        return $('<div class="round">').css('marginTop', dim.roundTop);
-      },
-      match:  function(dim) {
-        return $('<div class="match">').css('marginBottom', dim.matchGap);
-      },
-      player: function(dim) {
-        return $('<div class="player">Player</div>')
-               .css('height', dim.playerHeight);
-      }
-  };
-
-  var scaffoldMatch = function(dim) {
-    var p1 = html.player(dim);
-    p1.css('marginBottom', dim.playerGap);
-    return html.match(dim).append(p1).append(html.player(dim));
-  };
-
-  var roundOf = function(dim, num) {
-    var round, match, marginTop, newDim;
-    round = html.round(dim);
-
-    // create matches
-    for (var i = 0; i < num / 2; i++) {
-      match = scaffoldMatch(dim);
-      round.append(match);
+    mouseleave:function() {
+      var cls = $(this)[0].className;
+      // TODO - add backwards compatible #map
+      cls = cls.split(' ').map(function(n) { return "." + n; }).join("")
+      $(cls).removeClass('player-highlight');
     }
 
-    bracket.append(round);
-    return recalculateDimensions(dim);
-  };
-
-
-  var dim = dimensions;
-
-  dim = roundOf(dim, 128);
-  dim = roundOf(dim, 64);
-  dim = roundOf(dim, 32);
-  dim = roundOf(dim, 16);
-  dim = roundOf(dim, 8);
-  dim = roundOf(dim, 4);
-  roundOf(dim, 2);
+  });
 
 });
