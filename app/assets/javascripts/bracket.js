944fe29f3c0a8e198780a36980f16a0bfbe62402
Jakub Arnold <darthdeus@gmail.com>
2012-07-02 20:58:30 +0200

    Minor refactoring of the bracket

--- app/assets/javascripts/bracket.js
+++ app/assets/javascripts/bracket.js
@@ -1,51 +1,52 @@
-window.dimensions = {
-  playerHeight: 20,
-  playerBox: 26,
-  playerGap: 3,
-  matchGap: 10,
-  roundTop: 0
-};
+var bracketModule = angular.module('bracket', []);
 
-$.fn.applyDimensions = function(dim) {
-  var recalculateDimensions = function(dim) {
-    return {
-      playerHeight: dim.playerHeight,
-      playerBox: dim.playerBox,
-      playerGap: dim.playerGap + dim.playerBox + dim.matchGap + 4,
-      matchGap: dim.playerBox + dim.matchGap + dim.playerGap,
-      roundTop: (dim.playerBox / 2) + (dim.playerGap / 2) + dim.roundTop
-    };
-  };
+var BracketCtrl = function($scope, $http) {
 
-  $(this).find('.round').each(function(round) {
-    $(this).css('marginTop', dim.roundTop);
-
-    $(this).find('.player').css('height', dim.playerHeight);
-    var matches = $(this).find('.match');
-    matches.css('marginBottom', dim.matchGap);
-
-    matches.find('.player:first').css('marginBottom', dim.playerGap);
-    dim = recalculateDimensions(dim);
-  });
-}
-
-
-function BracketCtrl($scope, $http) {
+  $scope.inputResult = function(target) { debugger; };
 
   $http.get('/tournaments/1.json').success(function(data) {
     $scope.rounds = data.tournaments;
-    // TODO - make this better
-    setTimeout(function() {
-      // $scope.$watch('rounds', function() {
-      $('.bracket').applyDimensions(window.dimensions);
-      // });
-    }, 500);
+
+    setTimeout(function() { $('.bracket').applyDimensions(window.dimensions); }, 300);
 
   });
+};
 
-}
+BracketCtrl.$inject = ["$scope", "$http"];
+
+
 
 jQuery(function($) {
+  window.dimensions = {
+    playerHeight: 20,
+    playerBox: 26,
+    playerGap: 3,
+    matchGap: 10,
+    roundTop: 0
+  };
+
+  $.fn.applyDimensions = function(dim) {
+    var recalculateDimensions = function(dim) {
+      return {
+        playerHeight: dim.playerHeight,
+        playerBox: dim.playerBox,
+        playerGap: dim.playerGap + dim.playerBox + dim.matchGap + 4,
+        matchGap: dim.playerBox + dim.matchGap + dim.playerGap,
+        roundTop: (dim.playerBox / 2) + (dim.playerGap / 2) + dim.roundTop
+      };
+    };
+
+    $(this).find('.round').each(function(round) {
+      $(this).css('marginTop', dim.roundTop);
+
+      $(this).find('.player').css('height', dim.playerHeight);
+      var matches = $(this).find('.match');
+      matches.css('marginBottom', dim.matchGap);
+
+      matches.find('.player:first').css('marginBottom', dim.playerGap);
+      dim = recalculateDimensions(dim);
+    });
+  }
 
   $('.bracket .player').live({
     mouseenter: function() {
