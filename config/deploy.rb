721bc4cdd2c6c95e6280eef27f42cd7ed13e8ad4
Jakub Arnold <darthdeus@gmail.com>
2012-07-05 22:54:10 +0200

    Remove precompile if needed, assets will get compiled every time now

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,17 +54,17 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/bin/unicorn #{release_path}/bin/unicorn"
   end
 
-  # Recompile assets only if needed
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} -- vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
+  # # Recompile assets only if needed
+  # namespace :assets do
+  #   task :precompile, :roles => :web, :except => { :no_release => true } do
+  #     from = source.next_revision(current_revision)
+  #     if capture("cd #{latest_release} && #{source.local.log(from)} -- vendor/assets/ app/assets/ | wc -l").to_i > 0
+  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  #     else
+  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
+  #     end
+  #   end
+  # end
 
 end
 
