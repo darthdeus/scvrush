42bec0708e7705e18d6e530344ddf1357b8205c9
Jakub Arnold <darthdeus@gmail.com>
2012-07-19 17:21:55 +0200

    Add default image to home scene news

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,10 +2,9 @@ class PostDecorator < Draper::Base
   decorates :post
 
   def link_with_image
-    if post.featured_image
-      h.link_to post do
-        h.image_tag post.featured_image.url(:thumb)
-      end
+    h.link_to post do
+      h.image_tag "https://s3.amazonaws.com/scvrush/uploads/post/featured_image/thumb_100x100_dark.png"
+      #post.featured_image.url(:thumb)
     end
   end
 
