42af41cb5a35742dbea8b8740819247f01f0e8fb
Jakub Arnold <darthdeus@gmail.com>
2012-09-04 12:16:28 +0200

    Fix typo in http referer for redirect after changing bnet username

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -37,7 +37,7 @@ class SignupsController < ApplicationController
 
   def require_bnet_username
     unless current_user.has_bnet_username?
-      session[:redirect_back] = env["HTTP_REFERRER"]
+      session[:redirect_back] = env["HTTP_REFERER"]
       redirect_to edit_user_path(current_user), notice: "You can't participate in a tournament unless you fill in your Battle.net username and code."
     end
   end
