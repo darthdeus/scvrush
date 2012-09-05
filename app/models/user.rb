db9b7df4456f1a07518742320020b464ed8c025b
Jakub Arnold <darthdeus@gmail.com>
2012-09-05 13:40:34 +0200

    Make login case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -209,7 +209,7 @@ class User < ActiveRecord::Base
   before_create { generate_token(:auth_token) }
 
   def self.authenticate(username, password)
-    user = self.where('username = ? OR email = ?', username, username).first
+    user = self.where('username ILIKE ? OR email ILIKE ?', username, username).first
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
