7c282b97b5e7909332731f7dc0b95e3b3b6608f6
Jakub Arnold <darthdeus@gmail.com>
2012-09-29 17:38:07 +0200

    Fix typo in relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,7 +4,7 @@ class Relationship < ActiveRecord::Base
   scope :unrestricted, :conditions => ['relationships.restricted = ?', false]
 	
   scope :to_type, lambda { |rtype| rtype = [rtype].flatten.compact; rtype.size > 0 && { conditions: ['relationships.requestee_type IN (?)', [rtype].flatten] } || {} }
-  scope :from_type, lambda { rtype| rtype = [rtype].flatten.compact; rtype.size > 0 && { conditions: ['relationships.requestor_type IN (?)', [rtype].flatten] } || {} }
+  scope :from_type, lambda { |rtype| rtype = [rtype].flatten.compact; rtype.size > 0 && { conditions: ['relationships.requestor_type IN (?)', [rtype].flatten] } || {} }
 
 
   default_scope :order => 'created_at DESC'
