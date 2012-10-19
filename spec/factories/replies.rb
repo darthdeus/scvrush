9fd9aa9aa89f071ae723f0935cb5b071286901ae
Jakub Arnold <darthdeus@gmail.com>
2012-10-19 11:35:37 +0200

    Remove forum

--- spec/factories/replies.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :reply do
-    content 'Lorem ipsum dolor sit amet'
-    association :topic
-    association :user
-  end
-end
--- spec/factories/sections.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :section do
-    name "MyString"
-  end
-end
