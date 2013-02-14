9c1c096f2f4dbd515a2dd71eaad3a544f5b1b893
Jakub Arnold <darthdeus@gmail.com>
2013-02-14 15:38:12 +0100

    Fix mass_assignment vulnerabilities

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,12 +1,5 @@
 class BlogPost < ActiveRecord::Base
   validates_presence_of :title, :url, :order
-
   scope :recent, order('"order" DESC').limit(5)
-
-  before_save :expire_sidebar_cache
-
-  def expire_sidebar_cache
-    Rails.cache.delete('views/bloglinks')
-    true
-  end
+  attr_accessible :title, :url, :order
 end
