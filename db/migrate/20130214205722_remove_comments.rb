c182e78d432c4a53a68c08a0f0d591deb45c7483
Jakub Arnold <darthdeus@gmail.com>
2013-02-14 21:58:43 +0100

    Remove comments

---
+++ db/migrate/20130214205722_remove_comments.rb
@@ -0,0 +1,9 @@
+class RemoveComments < ActiveRecord::Migration
+  def up
+    drop_table :comments
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- spec/factories/comments.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :comment do
-    content "Lorem ipsum dolor sit amet"
-    association :post
-    association :user
-  end
-end
