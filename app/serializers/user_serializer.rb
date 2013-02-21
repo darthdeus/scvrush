0eeaf56d7af0f8942b1ba003095ff49742a9117a
Jakub Arnold <darthdeus@gmail.com>
2013-02-21 19:29:31 +0100

    Temporarily disable following/followers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,8 +5,8 @@ class UserSerializer < ActiveModel::Serializer
     Timeline.for_user(object).map(&:id)
   end
 
-  has_many :followers, embed: :ids
-  has_many :following, embed: :ids
+  # has_many :followers, embed: :ids
+  # has_many :following, embed: :ids
 
   def image
     object.avatar.url(:thumb)
