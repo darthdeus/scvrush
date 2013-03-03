ce65e3223379d39e018f40a163b41f505f45ae3e
Jakub Arnold <darthdeus@gmail.com>
2013-03-03 17:43:11 +0100

    Make images clickable

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Scvrush::Application.config.session_store :cookie_store, key: '_scvrush_session'
-require 'action_dispatch/middleware/session/dalli_store'
-Scvrush::Application.config.session_store :dalli_store
+require "action_dispatch/middleware/session/dalli_store"
+Scvrush::Application.config.session_store :dalli_store, key: "_scvrush_session"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
