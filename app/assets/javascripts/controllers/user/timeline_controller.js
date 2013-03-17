2cd9adb084fe9ce0faaae1e262d25827bd8f8f91
Jakub Arnold <darthdeus@gmail.com>
2013-03-17 15:51:42 +0100

    Fix users filter on timeline controller

--- app/assets/javascripts/controllers/user/timeline_controller.js
+++ app/assets/javascripts/controllers/user/timeline_controller.js
@@ -3,6 +3,10 @@ Scvrush.UserTimelineController = Ember.ObjectController.extend({
   init: function() {
     this._super();
     this.set("content", Scvrush.currentUser);
-  }
+  },
+
+  users: function() {
+    return Scvrush.User.filter();
+  }.property()
 
 });
