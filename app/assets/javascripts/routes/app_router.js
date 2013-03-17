ba06796801202acd0a61aa1818eed5ce347faaad
Jakub Arnold <darthdeus@gmail.com>
2013-03-17 11:58:53 +0100

    Add contact & meet scvrush in navbar

--- app/assets/javascripts/routes/app_router.js
+++ app/assets/javascripts/routes/app_router.js
@@ -4,6 +4,7 @@
 Scvrush.Router.map(function() {
   this.route("home", { path: "/" });
   this.route("meet_scvrush", { path: "/meet-scvrush" });
+  this.route("contact", { path: "/contact" });
 
   this.resource("posts", { path: "/posts" }, function() {
     this.route("post", { path: "/:post_id" });
