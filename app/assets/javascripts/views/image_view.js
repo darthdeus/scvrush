3585844c7ec68209d34afcefdadf363431a1bb0f
Jakub Arnold <darthdeus@gmail.com>
2013-03-20 19:00:02 +0100

    ImageView now only replaces the image once

--- app/assets/javascripts/views/image_view.js
+++ app/assets/javascripts/views/image_view.js
@@ -2,9 +2,17 @@ Scvrush.ImageView = Ember.View.extend({
   tagName: "img",
   attributeBindings: ["src"],
 
+  failed: false,
+
   didInsertElement: function() {
+    var self = this;
+
     this.$()[0].onerror = function() {
-      this.src = "https://s3.amazonaws.com/scvrush/uploads/post/featured_image/100x100_dark.png";
+      if (!self.get("failed")) {
+        self.set("failed", true);
+
+        this.src = "https://s3.amazonaws.com/scvrush/uploads/post/featured_image/100x100_dark.png";
+      }
     };
   }
 
