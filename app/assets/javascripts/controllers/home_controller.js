b3ba50885dba4826bcd1631e066cfcae7308c281
Jakub Arnold <darthdeus@gmail.com>
2013-03-22 14:35:05 +0100

    Add multi-step account activation

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,3 +1,11 @@
 Scvrush.HomeController = Ember.ObjectController.extend({
 
+  step2: function() {
+    return !!this.get("race");
+  }.property("race"),
+
+  step3: function() {
+    return !!this.get("server") && this.get("step2");
+  }.property("server")
+
 });
