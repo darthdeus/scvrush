6a2c459fc5f00ff195c7941da1ea2f28f336a185
Jakub Arnold <darthdeus@gmail.com>
2013-05-06 15:55:27 +0200

    Fix coaches link on dashboard

--- app/assets/javascripts/controllers/dashboard_controller.js
+++ app/assets/javascripts/controllers/dashboard_controller.js
@@ -1,10 +1,16 @@
 Scvrush.DashboardController = Ember.ObjectController.extend({
 
+  needs: "postsIndex",
   posts: null,
 
   init: function() {
     this._super();
     this.set("posts", Scvrush.Post.query());
+  },
+
+  gotoCoaches: function() {
+    this.transitionToRoute("posts");
+    this.set("controllers.postsIndex.query", Scvrush.currentUser.get("race") + " coach");
   }
 
 });
