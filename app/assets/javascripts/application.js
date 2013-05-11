0c7b6690c2e7493442f913f1540f5b514c49f73f
Jakub Arnold <darthdeus@gmail.com>
2013-05-11 13:56:00 +0200

    Fix bugsense onerror call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,6 @@ $(function() {
       title: "Something went wrong",
       text: "We've been notified about this, please reload the page. If the error persists, please contact us via our customer supprt or at jakub@scvrush.com"
     });
-    orig(arguments);
+    orig.apply(arguments);
   };
 });
