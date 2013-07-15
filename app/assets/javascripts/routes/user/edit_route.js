49c106147bc40e34ad307a897dab0c4972a91065
Jakub Arnold <darthdeus@gmail.com>
2013-07-15 12:05:32 +0200

    Fix user edit currentUser unwrap

--- app/assets/javascripts/routes/user/edit_route.js
+++ app/assets/javascripts/routes/user/edit_route.js
@@ -4,6 +4,14 @@ Scvrush.UserEditRoute = Scvrush.Route.extend({
     return this.modelFor("user");
   },
 
+  setupController: function(controller, model) {
+    if (model instanceof Scvrush.CurrentUser) {
+      controller.set("model", model.get("content"));
+    } else {
+      controller.set("model", model);
+    }
+  },
+
   redirect: function(model) {
     if (model.get("isTrial")) {
       this.transitionTo("user.activate", model);
