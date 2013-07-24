49c6bc33acbc0f5582959797b84ec61e2d68c3f2
Jakub Arnold <darthdeus@gmail.com>
2013-07-24 15:03:40 +0200

    Titleize user's race

--- app/assets/javascripts/vendor/manifest.js
+++ app/assets/javascripts/vendor/manifest.js
@@ -1,7 +1,8 @@
 //= require ./select2
 //= require ./Markdown.Converter
 //= require ./moment
-//= require ./underscore
+//= require underscore
+//= require underscore.string
 //= require ./jquery-ui-timepicker-addon
 //= require ./jquery.inview
 //= require ./catsocket
