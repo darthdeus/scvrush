d54fcb51ba182056daac3c585ab3a3a178391f39
Jakub Arnold <darthdeus@gmail.com>
2013-07-25 15:26:41 +0200

    Unique validate bnet_info

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,6 +105,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
   validates_presence_of :password, on: :create
+  validates :bnet_username, uniqueness: { scope: :bnet_code }
 
   def bnet_username_is_string
     if bnet_username?
