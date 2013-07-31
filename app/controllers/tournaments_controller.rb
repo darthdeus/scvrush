291d572140a4598860b038f4fbe74ebad40fd657
Jakub Arnold <darthdeus@gmail.com>
2013-07-31 03:34:12 +0200

    Add really basic calendar

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,6 +2,9 @@ class TournamentsController < AuthenticatedController
 
   def index
     @grouped_tournaments = Tournament.order("starts_at DESC").limit(20).group_by { |tournament| tournament.starts_at.to_date }
+    @calendar_tournaments = Tournament.order("starts_at DESC")
+                                        .where("created_at > ?", 1.month.ago)
+                                        .group_by { |tournament| tournament.starts_at.to_date }
   end
 
   def show
