0fe198780a006408a2d7906866a47a790d19e1e0
Jakub Arnold <darthdeus@gmail.com>
2013-08-02 16:36:57 +0200

    Cleanup asset production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,14 +13,19 @@ Scvrush::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = false
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # Generate digests for assets URLs
+  # Generate digests for assets URLs.
   config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
 
   config.assets.initialize_on_precompile = false
 
