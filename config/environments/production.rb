193adc1fd38fcbbd31088fbf16bcaa54fec96aab
Jakub Arnold <darthdeus@gmail.com>
2013-08-02 16:38:16 +0200

    Expire all assets, let's see if this helps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Scvrush::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
-
+  config.assets.version = "1.1"
 
   config.assets.initialize_on_precompile = false
 
