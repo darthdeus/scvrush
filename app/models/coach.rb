1bf34a62221045fc19665f7074f931e47eaa9a85
Jakub Arnold <darthdeus@gmail.com>
2013-08-03 11:58:30 +0200

    Fix coaches asset paths

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -3,11 +3,11 @@ class Coach < ActiveRecord::Base
   validates_presence_of :name, :about, :races, :servers, :languages
 
   def main_server_icon
-    "/assets/coaches_#{servers.first}.png"
+    "coaches_#{servers.first}.png"
   end
 
   def race_icons
-    races.map { |race| "/assets/race_#{race}_icon.png" }
+    races.map { |race| "race_#{race}_icon.png" }
   end
 
 end
