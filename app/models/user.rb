f07d413c4106d38e410378be2fbf1bf0a285fc72
Jakub Arnold <darthdeus@gmail.com>
2013-08-03 10:21:07 +0200

    Only display 3 suggested posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ActiveRecord::Base
   end
 
   def suggested_posts
-    race ? Post.search(race, limit: 10) : []
+    race ? Post.search(race, limit: 3) : []
   end
 
   # Return a user either by his username or by email.
