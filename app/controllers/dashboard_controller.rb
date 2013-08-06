7ddbb1e7390969ec0f3be2b11d35c4f3b25b9613
Jakub Arnold <darthdeus@gmail.com>
2013-08-06 16:19:39 +0200

    Fix upcoming tournaments on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < AuthenticatedController
 
   def index
-    @upcoming_tournaments = Tournament.order(:starts_at).limit(3)
+    @upcoming_tournaments = Tournament.order("starts_at DESC").limit(3)
   end
 
 end
